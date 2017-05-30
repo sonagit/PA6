06300d48e9c179986e2378e180220138cf9e1b88
Stu <supadupastu@gmail.com>
2017-05-30 14:44:58 -0700

    Add files via upload

---
+++ jcomp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo $'\n\n\n\n\n'
+
+javac -cp .:tester.jar *.java
+
+# Run TestTweetServer if test is passed
+if [ "$1" == "test" ]; then
+  java -cp $LIB/public/tester.jar:. tester.Main TestTweetServer
+fi
+
+# Run TweetServerMain if server is passed
+if [ "$1" == "server" ]; then
+  java TweetServerMain
+fi
